7b0e6023b10985076127bcd3508e29277af959b7
Deise Sales <deisesalles18@gmail.com>
2024-03-22 16:04:04 -0300

    3.2 update ColaboradorService.java
    correção da classe e seus métodos

--- escola/src/main/java/br/com/vainaweb/escola/service/ColaboradorService.java
+++ escola/src/main/java/br/com/vainaweb/escola/service/ColaboradorService.java
@@ -10,10 +10,6 @@ import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.List;
 
-@AllArgsConstructor
-@NoArgsConstructor
-@Getter
-@Setter
 @Service
 public class ColaboradorService {
     private List<ColaboradorService> lista = new ArrayList<>();
@@ -21,19 +17,4 @@ public class ColaboradorService {
     private String nome;
     private String cpf;
     private String endereco;
-
-    public ColaboradorService(Long id, String nome, String cpf, String endereco) {
-        this.id = id;
-        this.nome = nome;
-        this.cpf = cpf;
-        this.endereco = endereco;
-    }
-
-    public boolean cadastrarColaborador(ColaboradorResponseDTO dados) {
-        return lista.add(new ColaboradorService(dados.id(), dados.nome(), dados.cpf(), dados.cpf())) ;
-    }
-
-    public List<ColaboradorService> listarColaboradores() {
-        return lista;
-    }
 }
